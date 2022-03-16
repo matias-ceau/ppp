b8d969d496b9e12db3f69b6c955f56c3494d9b52
Matias Ceau <mceau@u862.local>
2022-03-16 16:04:36 +0100

    first upload

---
+++ pm.py
@@ -0,0 +1,126 @@
+import numpy as np
+import pandas as pd
+import sys
+import os
+import time
+import re
+import subprocess
+import argparse
+import yaml
+from tabulate import tabulate
+#==========================================================================================================
+
+class Practice:
+    """Blabla."""
+
+    def __init__(self):
+        with open('config.yaml') as f:
+            config  = yaml.load(f, Loader=yaml.FullLoader)
+            self.__dict__.update(config)
+        self.data   = self._getdata("practices.csv")
+        self.backup = self._getdata("backup.csv")
+
+    def _getdata(self,path):
+        if path not in os.listdir('.'):
+            return pd.DataFrame({i:[] for i in self.labels})
+        else:
+            return pd.read_csv(path)
+
+    def _save(self):
+        self.backup.to_csv('backup.csv',index=False)
+        self.data.to_csv("practices.csv",index=False)
+
+    def _niceprint(self):
+        not_nice = ['link','priority','created','log']
+        df = self.data.drop(columns=not_nice).tail(10)
+        print(tabulate(df, headers='keys', tablefmt='psql', showindex=False))
+
+    def add(self):
+        """Add practice session."""
+        self.ID = [i for i in range(1,1000) if i not in self.data['ID']][0]
+        t = {'ID'            : self.ID,
+             'category'      : self.category,
+             'name'          : self.name,
+             'instrument'    : self.instrument,
+             'tags'          : self.tags,
+             'description'   : self.description,
+             'link'          : self.link,
+             'priority'      : self.priority,
+             'status'        : self.status,
+             'created'       : [time.time()],
+             'log'           : [''],
+             'count'         : [0],
+             'goal'          : self.goal,
+             'urg'           : ['???????????']}
+        df = pd.DataFrame(t)
+        self.data = pd.concat([self.data,df],ignore_index=True)
+        self._save()
+        self._niceprint()
+        print('\n',f"Practice {self.ID} successfully created!")
+
+    def delete(self):
+        """Delete practice with inputed ID."""
+        self.backup = self.data.copy()
+        for i in self.ID:
+            if i in self.data['ID']:
+                self.data = self.data[self.data['ID'] != self.ID]
+            else:
+                print(f'No practice with ID {i}')
+        self._save()
+        print(self.data.tail(10))
+
+    def mod(self):
+        """Modify practice."""
+        return
+
+    def ls(self):
+        """List practices."""
+        pass
+
+    def practice(self):
+        """Choose practice or get weighted random practice and launch it."""
+        if input('Choose practice?\n').upper() in 'YES':
+            subprocess.run(f'xdg-open {self.link}'.split(' '))
+
+    def stats(self):
+        """Give stats about practising."""
+        return
+
+    def undo(self):
+        """Return to backup.csv."""
+        subprocess.run('mv backup.csv temp.csv'.split(' '))
+        subprocess.run('mv practices.csv backup.csv'.split(' '))
+        subprocess.run('mv temp.csv practices.csv'.split(' '))
+
+#==========================================================================================
+
+practice = Practice()
+parser = argparse.ArgumentParser(prog=('PPP'),
+                                 description='PracticePracticePractice (ppp)',
+                                 epilog='this is the epilog')
+parser.add_argument('cmd',choices = practice.cmds,nargs='?',default='add')
+parser.add_argument('-0', '--ID', type=int)
+parser.add_argument('-c','--category',choices = practice.categories)
+parser.add_argument('-n','--name')
+parser.add_argument('-i','--instrument',choices = practice.instruments.values())
+parser.add_argument('-t','--tags')
+parser.add_argument('-d','--description') #nargs???
+parser.add_argument('-l','--link')
+parser.add_argument('-p','--priority')
+parser.add_argument('-v', '--verbose')
+parser.add_argument('-s','--status')
+parser.add_argument('-g', '--goal')
+parser.add_argument('-u','--urgency')
+parser.add_argument('--created')
+parser.add_argument('--log')
+parser.add_argument('--count')
+
+a = parser.parse_args(namespace=practice)
+# print(practice.ID)
+# print(practice.category)
+# print(practice.name)
+# print(practice.link)
+# print(practice.instrument)
+# print(practice.created)
+# print(practice.count)
+Practice.__dict__[practice.cmd](practice)
